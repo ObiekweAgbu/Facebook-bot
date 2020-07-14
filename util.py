3546ccc87115c31337744954d1f26e1ae761da45
ObiekweAgbu <49278842+ObiekweAgbu@users.noreply.github.com>
2020-07-14 14:35:06 +0100

    first commit

---
+++ util.py
@@ -0,0 +1,50 @@
+import os, sys
+from flask import Flask, request
+from pymessenger import Bot
+util = Flask(__name__)
+
+PAGE_ACCESS_TOKEN = "EAAl43ZAebPSwBALlJhGdtWw4BXh8vrl0TaIarEykSDyXVPzSx2ZBBGa2ZBZBjZBzKilTP7lfa73iHZCZBJ7OQWfm9czZBrjrg2Ggr6z1ieuIsAgKvyzBLZA0yEfGOGeIdpm8Jjm8JM70ZBrJpenjvMiWcwHyinaGYz9bRBjmJexwk2RUPahjb56Wy9S0DaH72KqMwZD"
+
+bot = Bot(PAGE_ACCESS_TOKEN)
+
+
+@util.route('/', methods=['GET'])
+def verify():
+    # Webhook verification
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == "hello":
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
+    return "Hello world", 200
+
+
+@util.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    log(data)
+
+    if data['object'] == 'page':
+        for entry in data['entry']:
+            for messaging_event in entry['messaging']:
+                # IDs
+                sender_id = messaging_event['sender']['id']
+                recipient_id = messaging_event['recipient']['id']
+                if messaging_event.get('message'):
+                    if 'text' in messaging_event['message']:
+                        messaging_text = messaging_event['message']['text']
+                    else:
+                        messaging_text = 'no text'
+
+                    # Echo
+                    response = messaging_text
+                    bot.send_text_message(sender_id, response)
+    return "ok", 200
+
+
+def log(message):
+    print(message)
+    sys.stdout.flush()
+
+
+if __name__ == "__main__":
+    util.run(debug=True, port=80)
