37b54238b885daef1826992bf5665b7497e7762b
ObiekweAgbu <49278842+ObiekweAgbu@users.noreply.github.com>
2020-07-15 15:58:27 +0100

    response

--- util.py
+++ util.py
@@ -1,6 +1,8 @@
 import os, sys
 from flask import Flask, request
+from wits import wit_response
 from pymessenger import Bot
+
 util = Flask(__name__)
 
 PAGE_ACCESS_TOKEN = "EAAl43ZAebPSwBALlJhGdtWw4BXh8vrl0TaIarEykSDyXVPzSx2ZBBGa2ZBZBjZBzKilTP7lfa73iHZCZBJ7OQWfm9czZBrjrg2Ggr6z1ieuIsAgKvyzBLZA0yEfGOGeIdpm8Jjm8JM70ZBrJpenjvMiWcwHyinaGYz9bRBjmJexwk2RUPahjb56Wy9S0DaH72KqMwZD"
@@ -35,8 +37,16 @@ def webhook():
                     else:
                         messaging_text = 'no text'
 
-                    # Echo
-                    response = messaging_text
+                    response = None
+                    entity, value = wit_response(messaging_text)
+
+                    if entity == 'news':
+                        response = "Ok I will snd you {} news".format(str(value))
+                    elif entity == "location":
+                        response = "You live in {0}. I will send you top headlines from {0}".format(str(value))
+
+                    if response == None:
+                        response = "Sorry I don't understand that"
                     bot.send_text_message(sender_id, response)
     return "ok", 200
 
