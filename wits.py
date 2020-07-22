c8db50d728bc86593ff5dd72434e52774e5fdaeb
ObiekweAgbu <49278842+ObiekweAgbu@users.noreply.github.com>
2020-07-22 13:42:26 +0100

    t

--- wits.py
+++ wits.py
@@ -41,7 +41,7 @@ def wit_response(message_text):
     except:
         pass
 
-    return(entity.get('product:product'), trait)
+    return(entity, trait)
 
 
 print(wit_response("How much would it cost for a hedge trim"))
